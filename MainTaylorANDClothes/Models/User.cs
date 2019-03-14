1273daf3ceedfdcd09aa2c319245200baf3afa4a
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-03-14 23:42:10 +0500

    Update Added User Class for Login

---
+++ MainTaylorANDClothes/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace MainTaylorANDClothes.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string LoginId { get; set; }
+        public string Password { get; set; }
+
+    }
+}
