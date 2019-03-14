b6bbf3f804d2fe5bc0baf5a411cce943e0c6f5c6
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-03-14 23:40:21 +0500

    Update Added New Users Controller

---
+++ MainTaylorANDClothes/Controllers/UsersController.cs
@@ -0,0 +1,65 @@
+﻿using MainTaylorANDClothes.Models;
+using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MainTaylorANDClothes.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            HttpCookie myCookie = Request.Cookies["logsec"];
+            if (myCookie != null)
+            {
+                User user = new UserHandler().GetUser(myCookie.Values["logid"], myCookie.Values["psd"]);
+                if (user != null)
+                {
+                    myCookie.Expires = DateTime.Now.AddDays(7);
+
+                }
+            }
+            ViewBag.Controller = Request.QueryString["ctl"];
+            ViewBag.Action = Request.QueryString["act"];
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(User model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            User u = new UserHandler().GetUser(model.LoginId, model.Password);
+            if (u != null)
+            {
+
+                HttpCookie cookie = new HttpCookie("logsec")
+                { Expires = DateTime.Now.AddDays(7) };
+                cookie.Values.Add("logid", u.LoginId);
+                cookie.Values.Add("psd", u.Password);
+                Response.SetCookie(cookie);
+
+
+                string ctl = Request.QueryString["c"];
+                string act = Request.QueryString["a"];
+                if (!string.IsNullOrEmpty(ctl) && string.IsNullOrEmpty(act))
+                {
+                    return RedirectToAction(ctl, act);
+                }
+                return RedirectToAction("Index", "Taylors");
+            }
+            else
+            {
+                ViewBag.Error = "Your LoginId and Password are Wrong..Please try Again!";
+            }
+            return View();
+        }
+    }
+}
