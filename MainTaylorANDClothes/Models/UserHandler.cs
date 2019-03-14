2d3b0c5c2dd180adcd9d3608f9837a491eaa1262
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-03-14 23:42:38 +0500

    Udpate Added USerHandler  Class Added Different Methods

---
+++ MainTaylorANDClothes/Models/UserHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace MainTaylorANDClothes.Models
+{
+    public class UserHandler
+    {
+        public User GetUser(string Loginid, string Password)
+        {
+            TaylorContext db = new TaylorContext();
+            using (db)
+            {
+                return (from v in db.Users where v.LoginId.Equals(Loginid) && v.Password.Equals(Password) select v).FirstOrDefault();
+            }
+        }
+    }
+}
