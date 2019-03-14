edab2861e67862036f0b5a06c4d26f73a34eddec
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-03-14 23:41:51 +0500

    Update added New Database Context File

---
+++ MainTaylorANDClothes/Models/TaylorContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace MainTaylorANDClothes.Models
+{
+    public class TaylorContext : DbContext
+    {
+        public TaylorContext() : base("name=constr")
+        {
+
+        }
+
+        public DbSet<Taylor> Taylors { get; set; }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
