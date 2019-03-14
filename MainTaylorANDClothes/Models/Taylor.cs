e9c03ebd2a59b8832ef754ff97288144461e2264
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-03-14 23:41:25 +0500

    Update Added Taylor class Conatin all the property or attribute of the tables

---
+++ MainTaylorANDClothes/Models/Taylor.cs
@@ -0,0 +1,35 @@
+﻿namespace MainTaylorANDClothes.Models
+{
+    public class Taylor
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public double CellNo { get; set; }
+        public int KamezLenght { get; set; }
+        public int Tera { get; set; }
+        public int Galla { get; set; }
+        public int Kandha { get; set; }
+        public int Baghal { get; set; }
+        public int Bazoo { get; set; }
+        public int Kohni { get; set; }
+        public int Chest { get; set; }
+        public int Kamar { get; set; }
+        public int Ghera { get; set; }
+        public int ShalwaarLenght { get; set; }
+        public int Aasan { get; set; }
+        public int Ponchha { get; set; }
+        public int Palla { get; set; }
+        public int Kundha { get; set; }
+        public int Kundhi { get; set; }
+        public int Saali { get; set; }
+        public int Kaff { get; set; }
+        public string Button { get; set; }
+        public string Pocket { get; set; }
+        public string Sticker { get; set; }
+        public int TotalSuit { get; set; }
+        public string Gheras { get; set; }
+
+
+
+    }
+}
